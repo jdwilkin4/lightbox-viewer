f28f099a01b95c91f7d17c4eddec6d0690f0084b
jdwilkin4 <jwilkin4@hotmail.com>
2025-04-12 18:49:41 -0700

    chore: updating lightbox tests

--- tests/lightbox.spec.js
+++ tests/lightbox.spec.js
@@ -1,12 +1,12 @@
 import { test, expect } from "@playwright/test";
 
-// TODO: finish lightbox tests. Needs to test for specific changes in the images.
-
 test("users should be able to click on the featured image to open the lightbox", async ({
   page,
 }) => {
   await page.goto("http://localhost:1234/");
-  await page.locator("#office-chair-1").click();
+  const firstOfficeChairImg = page.locator("#office-chair-1");
+  await expect(firstOfficeChairImg).toBeVisible();
+  await firstOfficeChairImg.click();
   page.locator("div").filter({ hasText: "× ⇦ ⇨" }).getByRole("img");
   await page.getByRole("button", { name: "×" }).click();
 });
@@ -15,7 +15,9 @@ test("users should be able to open the lightbox and cycle through the different 
   page,
 }) => {
   await page.goto("http://localhost:1234/");
-  await page.locator("#office-chair-1").click();
+  const firstOfficeChairImg = page.locator("#office-chair-1");
+  await expect(firstOfficeChairImg).toBeVisible();
+  await firstOfficeChairImg.click();
   await page
     .locator("div")
     .filter({ hasText: "× ⇦ ⇨" })
